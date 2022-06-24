637eca8a36d926688b5f585dd08f364730cc24a8
JeongHoon Lee <sherry0_@kakao.com>
2022-06-24 22:34:12 +0900

    test

--- calculator.py
+++ calculator.py
@@ -1,30 +1,38 @@
 # 기본 계산기
 def add(a, b):
-	return a+b
+    return a+b
+
 
 def subtract(a, b):
-	return a-b
+    return a-b
+
 
 def multiply(a, c):
-	return a*c
+    return a*c
+
 
 def divide_new(a, b):
-	return a/b
+    return a/b
+
 
 def median(a, b):
-	return (a*b)/2
+    return (a*b)/2
+
 
 def remainder(a, b):
-	retunr a//b
+    retunr a//b
+
 
 def Abs(num):
-	if num>=0:
-		return num
-	else:
-		return -num
+    if num >= 0:
+        return num
+    else:
+        return -numaa
+
 
 def percent(a, b):
-	return (a/b)*100
+    return (a/b)*100
+
 
 def get_Sum_ver1(n):
-	return n(n+1)/2
+    return n(n+1)/2
